b7bcbd9e91adda51b37b20343296fdac6d3d77f2
LucasDachman <lucas.dachman@gmail.com>
2017-04-27 19:27:14 -0600

    fixed semi colon

--- geometry.c
+++ geometry.c
@@ -23,7 +23,7 @@ double coord_2d_area_triangle(const coord_2d_t *a, const
 				 coord_2d_t *b, const coord_2d_t *c) {
 
 	double area;
-	area =( a->x*(b->y - c->y) + b->x*(c->y - a->y) + c->x*(a->y - b->y) )/2
+	area =( a->x*(b->y - c->y) + b->x*(c->y - a->y) + c->x*(a->y - b->y) )/2;
 	return area;
 }
 
